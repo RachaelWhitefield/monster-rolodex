3a3baba2dae41f5dfda77b5f6326d69738459b8e
Fabulous_Rachael <rachael.whitefield@gmail.com>
2020-03-13 15:28:18 -0600

    Updates

---
+++ precache-manifest.e72679fd41a82043b669283cbdee4653.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "519cb8a67e2c11f28ad92180c02442ec",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "ec39de3e759071fc9c5f",
+    "url": "/monster-rolodex/static/css/main.82f9d073.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/monster-rolodex/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monster-rolodex/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec39de3e759071fc9c5f",
+    "url": "/monster-rolodex/static/js/main.f74a0a1f.chunk.js"
+  },
+  {
+    "revision": "bc09d86fba44e1c7ba83",
+    "url": "/monster-rolodex/static/js/runtime-main.8000f3b5.js"
+  }
+]);
