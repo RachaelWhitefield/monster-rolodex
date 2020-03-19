050dde56a15698b31e2b6ff46db2a545647105af
Fabulous_Rachael <rachael.whitefield@gmail.com>
2020-03-19 17:13:45 -0600

    commented my code

--- src/App.js
+++ src/App.js
@@ -5,28 +5,28 @@ import './App.css';
 
 class App extends Component {
   constructor() {
-    super();
+    super(); // Calls the constructor method on the component class.  Gives access to this.state
 
-    this.state = {
-      monsters: [],
-      searchField: ""
+    this.state = { // gets passed down to the card-list component as an attribute
+      monsters: [], // getting json user data array returned from api
+      searchField: "" // an empty string to start the search
     };
   }
 
-  componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
-    .then(response => response.json())
-    .then(users => this.setState({ monsters: users }));
+  componentDidMount() { // when component is first on the page, run this code below
+    fetch("https://jsonplaceholder.typicode.com/users") // these are our random users from the api
+    .then(response => response.json())  // getting data back from api and turning it into info we can read
+    .then(users => this.setState({ monsters: users })); // re-naming users to monsters
   }
 
-  handleChange = (e) => {
-    this.setState({ searchField: e.target.value });
+  handleChange = e => {
+    this.setState({ searchField: e.target.value }); // tracking what is typed to check against array member monsters
   }
 
 
   render() {
-    const { monsters, searchField } = this.state;
-    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase())
+    const { monsters, searchField } = this.state; // destructuring the properties from above into constants
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()) // filtering monster name by turning all search characters into lowercase and get back new filtered array
     );
 
     return (
@@ -34,9 +34,9 @@ class App extends Component {
         <h1> Monsters Rolodex </h1>
         <SearchBox
           placeholder="Search Monsters"
-          handleChange={this.handleChange}
+          handleChange={this.handleChange} // refilters everytime the cardlist is rerendered
         />
-        <CardList monsters={filteredMonsters} />
+    <CardList monsters={filteredMonsters} /> {/* cardlist is returning filter from above search */}
       </div>
     );
   }
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -3,8 +3,8 @@ import "./card.styles.css";
 
 export const Card = (props) => (
     <div className="card-container">
-        <img alt="monster" src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`} />
-        <h2> {props.monster.name} </h2>
+        <img alt="monster" src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`} /> {/* This is the api where our monster pictures come from as well as setting the size we want */}
+        <h2> {props.monster.name} </h2>{/* The json info getting passed down from the api call */}
         <p> {props.monster.email} </p>
     </div>
 )
